714c2333cfb5fac22553e8640bdbebb0bbc5f2e8
Valera Viachalo <valeravyachalo@gmail.com>
2023-11-19 17:59:02 +0200

    header menu for mobile (try 1)

--- src/components/Header/Navigation/Navigation.tsx
+++ src/components/Header/Navigation/Navigation.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { FC, useState } from "react";
+import { FC, useEffect, useState } from "react";
 import { AnimatePresence, motion } from "framer-motion";
 import "./Navigation.scss";
 import NavList from "./NavList/NavList";
@@ -9,10 +9,47 @@ type BtnProps = {
   toggleMenu: () => void;
 };
 
+// const menu = {
+//   open: {
+//     width: "max-content",
+//     height: "max-content",
+//     left: '-1.6vw',
+//     transition: {
+//       duration: 0.8,
+//       type: "tween",
+//       ease: [0.76, 0, 0.24, 1]
+//     },
+//   },
+//   closed: {
+//     width: "10px",
+//     height: "10px",
+//     transition: {
+//       duration: 0.8,
+//       delay: 0.45,
+//       type: "tween",
+//       ease: [0.76, 0, 0.24, 1],
+//     },
+//     transitionEnd: {
+//       display: 'none'
+//     }
+//   },
+// };
+
+export default function Navigation() {
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+useEffect(() => {
+  const handleResize = () => setWindowWidth(window.innerWidth);
+  window.addEventListener('resize', handleResize);
+  return () => {
+    window.removeEventListener('resize', handleResize);
+  };
+}, []);
+
 const menu = {
   open: {
     width: "max-content",
-    height: "max-content",
+    height: windowWidth <= 768 ? "70vw" : "max-content", // change "60vh" to the height you want on mobile
     left: '-1.6vw',
     transition: {
       duration: 0.8,
@@ -35,7 +72,6 @@ const menu = {
   },
 };
 
-export default function Navigation() {
   const [isActive, setIsActive] = useState(false);
 
   return (
